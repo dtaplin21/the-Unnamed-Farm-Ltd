d334717bdcfc413b3fd0d27abbf52667cd2a2e81
dominique Taplin <Dtaplin21@aol.com>
2025-01-17 12:05:11 -0800

    new updates

--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -46,12 +46,17 @@ const Team = () => {
                                 <span>Washington, DC</span>
                             </div>
                         </div>
-                        <p>A proud Liberian native, Jenkins is an alumnus of Georgtown prepartiry 
-                            school and furthered his education at the University of Maryland, College Park. 
-                            There, he not only excelled as a Division 1 restler but also delved into businedss studies at the esteemed 
-                            Robert H Smth Business School. Post College Jenkins has been a successful entrepreneur with the inception 
-                            of a drop-shipping venture follwed by a digital production company. Drawing from his rich lineage of farmers 
-                            and his prowess in marketing, co-founded The unnamed Farm LTD with his father aiming to spotlight the challenges faced by Liberian farmers.</p>
+                        <p>A proud Liberian native, Jenkins is an alumnus of Georgetown 
+                            Preparatory School and furthered his education at the 
+                            University of Maryland, College Park. There, he not only 
+                            excelled as a Division 1 wrestler but also pursued business 
+                            studies at the esteemed Robert H. Smith School of Business. 
+                            After college, Jenkins became a successful entrepreneur, 
+                            starting with a drop-shipping venture followed by a digital 
+                            production company. Drawing from his rich lineage of farmers 
+                            and his expertise in marketing, he co-founded The Unnamed Farm LTD 
+                            with his father, aiming to spotlight the challenges faced by 
+                            Liberian farmers.</p>
                     </div>
                 </li>
                 <li>
@@ -63,15 +68,17 @@ const Team = () => {
                                 <span>Washington, DC</span>
                             </div>
                         </div>
-                        <p>Born in Congo-Kinshasa, Zathy Ndiang's early life 
-                            was marked by resilience as he and his family sought 
-                            refuge in Columbia, MD, escaping the turmoil of a civil war. 
-                            Growing up in Howard County's diverse milieu, Zathy honed his 
-                            ability to connect with individuals from varied backgrounds. 
-                            This skill translated into a flourishing career in automotive, f
-                            inancial, and software sales. Joining The UnNamed Farm Inc., 
-                            Zathy leverages his sales acumen to spearhead sales initiatives 
-                            and foster relationships, fortifying the brand's presence in the market.</p>
+                        <p>Born in Congo-Kinshasa, Zathy Ndiang's early life was marked 
+                            by resilience as he and his family sought refuge in Columbia,
+                             Maryland, escaping the turmoil of a civil war. Growing up 
+                             in Howard County's diverse community, Zathy developed a 
+                             strong ability to connect with people from varied 
+                             backgrounds. This skill laid the foundation for a 
+                             successful career in automotive, financial, and software 
+                             sales. As a key member of The Unnamed Farm Inc., 
+                             Zathy leverages his sales expertise to lead sales 
+                             initiatives and build lasting relationships, strengthening 
+                             the brand's presence in the market.</p>
                     </div>
                 </li>
                  <li>
@@ -88,11 +95,12 @@ const Team = () => {
                             Dominican University of California, is a seasoned Project 
                             Manager at SDAC Lining Group LLC. His four-year tenure has 
                             honed his expertise in data analytics, budget management, 
-                            risk assessment, and mitigation. Dom's role demands meticulous 
-                            coordination with manufacturing entities, adept contract 
-                            negotiations, and the implementation of cost-efficient strategies. 
-                            His agility in adapting to evolving scenarios, coupled with his 
-                            software development acumen, makes him an invaluable asset in project leadership.</p>
+                            risk assessment, and mitigation. Dominique's role demands 
+                            meticulous coordination with manufacturing entities, adept 
+                            contract negotiation, and the implementation of cost-efficient 
+                            strategies. His agility in adapting to evolving scenarios, 
+                            coupled with his software development expertise, makes him 
+                            an invaluable asset in project leadership.</p>
                     </div>
                 </li>
                  <li>
@@ -104,16 +112,17 @@ const Team = () => {
                                 <span>Washington, DC</span>
                             </div>
                         </div>
-                        <p>Darcy Pelz-Butler, a cornerstone of The UnNamed Farm Inc., boasts a 
-                            diverse background rooted in athletics and multiculturalism. 
-                            A Division 1 soccer player at Delaware State University, 
-                            she graduated suma cum laude with a Bachelor of Science in Physical Education. 
-                            Darcy's passion for youth development saw her coaching, organizing summer camps, 
-                            and engaging in early childhood education. Her career trajectory took a turn when 
-                            she joined Arthritis and Rheumatism Associates PC, one of America's premier Rheumatology 
-                            practices. Starting as a physical therapy technician, her dedication saw her rise to the role of 
-                            Front Office Coordinator. Darcy's commitment to physical well-being aligns seamlessly with The UnNamed Farm Inc.'s 
-                            vision of promoting health through nutrition.</p>
+                        <p>Darcy Pelz-Butler, a cornerstone of The Unnamed Farm Inc., 
+                            boasts a diverse background rooted in athletics and multiculturalism. 
+                            A Division 1 soccer player at Delaware State University, she graduated 
+                            summa cum laude with a Bachelor of Science in Physical Education. 
+                            Darcy's passion for youth development led her to coaching, organizing 
+                            summer camps, and engaging in early childhood education. Her career 
+                            trajectory shifted when she joined Arthritis and Rheumatism Associates, 
+                            P.C., one of America's premier rheumatology practices. Starting as a 
+                            physical therapy technician, her dedication propelled her to the role of 
+                            Front Office Coordinator. Darcy's commitment to physical well-being aligns 
+                            seamlessly with The Unnamed Farm Inc.'s vision of promoting health through nutrition.</p>
                     </div>
                 </li>
                 <li>
